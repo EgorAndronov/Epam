c323cbe4ff31ad6b99e6ae766989838c3a359926
AndronovEgor <andronov.egor.96@mail.ru>
2018-12-06 09:23:08 +0400

    Add User

---
+++ Epam.Task3/Epam.Task3.User_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task3.User_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter surName: ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Enter firstName: ");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("Enter patronymic: ");
+            string patronymic = Console.ReadLine();
+            Console.WriteLine("Enter Date of Birth: ");
+            Console.Write("Year:");
+            if (!int.TryParse(Console.ReadLine(), out int year))
+            {
+                Console.WriteLine("You entered not a number");
+            }
+
+            Console.Write("Month:");
+            if (!int.TryParse(Console.ReadLine(), out int month))
+            {
+                Console.WriteLine("You entered not a number");
+            }
+
+            Console.Write("Day:");
+            if (!int.TryParse(Console.ReadLine(), out int day))
+            {
+                Console.WriteLine("You entered not a number");
+            }
+
+            DateTime dateOfBirth = new DateTime(year, month, day);
+            User user = new User(surname, firstname, patronymic, dateOfBirth);
+
+            user.Display();
+        }
+    }
+}
---
+++ Epam.Task3/Epam.Task3.User_1/User.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task3.User_1
+{
+    class User
+    {
+        private string surName = string.Empty;
+        private string firstName = string.Empty;
+        private string patronymic = string.Empty;
+        private DateTime dateOfBirth;
+
+        public User()
+        {
+        }
+
+        public User(string surname, string firstname, string patronymic, DateTime date)
+        {
+            this.SurName = surname;
+            this.FirstName = firstname;
+            this.Patronymic = patronymic;
+            this.DateOfBirth = date;
+        }
+
+        public string SurName
+        {
+            get
+            {
+                return this.surName;
+            }
+
+            set
+            {
+                this.surName = value;
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+
+            set
+            {
+                this.firstName = value;
+            }
+        }
+
+        public string Patronymic
+        {
+            get
+            {
+                return this.patronymic;
+            }
+
+            set
+            {
+                this.patronymic = value;
+            }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get
+            {
+                return this.dateOfBirth;
+            }
+
+            set
+            {
+                this.dateOfBirth = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return DateTime.Now.Year - this.DateOfBirth.Year;
+            }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Фамилия: {SurName}, имя: {FirstName}, отчество: {Patronymic}, дата рождения: {DateOfBirth.ToString("d")}, возраст: {Age}");
+        }
+    }
+}
